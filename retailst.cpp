e9a027c3f679375e9b4824f6a5277da06bf9c791
zahidgzp80 <zahidgzp80@gmail.com>
2022-06-01 13:48:53 +0530

    Create retailst.cpp

---
+++ retailst.cpp
@@ -0,0 +1,28 @@
+#include <iostream>  
+#include <string>
+using namespace std;  
+
+class store
+{
+private:
+    int model;
+    float price;
+    float disc;
+    string prod_name;
+
+    public:
+    void menu();
+    void administrator();
+    void buyer();
+    void add();
+    void edit();
+    void list();
+    void receipt();
+    void remove();
+};
+
+int main()  
+{  
+   
+  return 0;  
+}  
