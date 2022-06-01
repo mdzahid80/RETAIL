90c6dfe8bd3fa8074b5ed9d84cb691f8c8b7e60e
zahidgzp80 <zahidgzp80@gmail.com>
2022-06-01 20:35:52 +0530

    1st Function

--- retailst.cpp
+++ retailst.cpp
@@ -1,5 +1,6 @@
-#include <iostream>  
-#include <string>
+#include <iostream> 
+#include <fstream> 
+
 using namespace std;  
 
 class shopping
@@ -20,7 +21,46 @@ private:
     void receipt();
 };
 
-voivd shopping :: menu()
+void shopping :: menu()
+{
+  int choice;
+  string email;
+  string password;
+
+  cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\tSUPERMARKET MENU\n";
+  cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\t________________\n";
+   cout<<"\t\t\t\t               \n";
+  cout<<"\t\t\t\t|1) Administrator\n";
+   cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\t|2) Buyer\n";
+   cout<<"\t\t\t\t________________\n";
+  cout<<"\t\t\t\t|3) Exit\n";
+
+  cout<<"\n\t Please select!";
+  cin>>choice;
+switch(choice)
+{
+  case 1:
+  cout<<"Please login\n";
+  cout<<"enter email\n";
+  cin>>email;
+  cout<<"enter password\n";
+  cin>>password;
+
+  if (email=="robby@gmail.com" && password=="zahid1122")
+  {
+    administrator();
+  };
+  
+ };
+  
+}
+
 
 
 
